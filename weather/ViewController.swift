003ac442fa8b3b4b622d6bdcee1c696db6935a1f
JinLang <tommyhilinux@gmail.com>
2014-12-28 02:24:27 +0800

    CLLocation

--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -7,17 +7,45 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    let locationManger:CLLocationManager = CLLocationManager()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManger.delegate = self
+        locationManger.desiredAccuracy = kCLLocationAccuracyBest
+        
+        if(ios8()){
+            locationManger.requestAlwaysAuthorization()
+        }
+        locationManger.stopUpdatingLocation()
+    }
+    func ios8() ->Bool {
+        return UIDevice.currentDevice().systemVersion == "8.0"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!){
+        var location:CLLocation = locations[locations.count-1] as CLLocation
+        
+        if(location.horizontalAccuracy > 0){
+            println(location.coordinate.latitude)
+            println(location.coordinate.longitude)
+            
+            locationManger.stopUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, rangingBeaconsDidFailForRegion region: CLBeaconRegion!, withError error: NSError!){
+        println(error)
     }
 
 
